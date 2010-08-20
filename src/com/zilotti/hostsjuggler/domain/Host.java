baab5bc7cb6944e7b9b77aa314a68c4804b179b0
Ivan Alencar <ialencar@gmail.com>
2010-08-20 19:45:13 -0400

    Testing new repository.

---
+++ src/com/zilotti/hostsjuggler/domain/Host.java
@@ -0,0 +1,15 @@
+package com.zilotti.hostsjuggler.domain;
+
+import java.io.Serializable;
+
+/**
+ * @author ialencar
+ *
+ */
+public class Host implements Serializable
+{
+	/** Serial version UID */
+	private static final long serialVersionUID = 1L;
+
+	
+}
