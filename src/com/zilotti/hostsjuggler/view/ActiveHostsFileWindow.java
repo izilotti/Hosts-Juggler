0446c5bdd39ed6eaad2e6e4587906870e1a50577
Ivan Alencar <ialencar@gmail.com>
2010-12-19 17:36:45 -0500

    Test.

--- src/com/zilotti/hostsjuggler/view/ActiveHostsFileWindow.java
+++ src/com/zilotti/hostsjuggler/view/ActiveHostsFileWindow.java
@@ -39,9 +39,7 @@ import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Shell;
@@ -49,6 +47,8 @@ import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
 
+import com.zilotti.hostsjuggler.dao.HostsFileDAO;
+import com.zilotti.hostsjuggler.dao.impl.HostsFileSerializationDAO;
 import com.zilotti.hostsjuggler.domain.BlankLine;
 import com.zilotti.hostsjuggler.domain.CommentLine;
 import com.zilotti.hostsjuggler.domain.HostLine;
@@ -107,6 +107,9 @@ public class ActiveHostsFileWindow
 		
 		System.out.println("Lines: "+ hf.getHostsFileLines().size());
 		
+		HostsFileDAO hfDAO = new HostsFileSerializationDAO();
+		hfDAO.save(hf);
+		
 		
 		while(!thisClass.sShell.isDisposed())
 		{
@@ -129,15 +132,71 @@ public class ActiveHostsFileWindow
 		/*
 		 * Menu
 		 */
-		menuBar = new Menu(sShell, SWT.BAR);
-		sShell.setMenuBar(menuBar);
+		final Menu m = new Menu(sShell, SWT.BAR);
+		sShell.setMenuBar(m);
 		
 		/* Hosts File */
-//		final MenuItem hostsFileMenuItem = new MenuItem(menuBar, SWT.CASCADE); 
-//		hostsFileMenuItem.setText("&Hosts File");
-//		final Menu hostsFileMenu = new Menu(sShell, SWT.DROP_DOWN);
-//		hostsFileMenu.se
-		
+		// create a file menu and add an exit item
+	    final MenuItem file = new MenuItem(m, SWT.CASCADE);
+	    file.setText("&Hosts File");
+	    final Menu filemenu = new Menu(sShell, SWT.DROP_DOWN);
+	    file.setMenu(filemenu);
+	    final MenuItem openItem = new MenuItem(filemenu, SWT.CASCADE);
+	    openItem.setText("&Open");
+	    final Menu submenu = new Menu(sShell, SWT.DROP_DOWN);
+	    openItem.setMenu(submenu);
+	    final MenuItem childItem = new MenuItem(submenu, SWT.PUSH);
+	    childItem.setText("&Child\tCTRL+C");
+	    childItem.setAccelerator(SWT.CTRL + 'C');
+	    final MenuItem dialogItem = new MenuItem(submenu, SWT.PUSH);
+	    dialogItem.setText("&Dialog\tCTRL+D");
+	    dialogItem.setAccelerator(SWT.CTRL + 'D');
+	    final MenuItem separator = new MenuItem(filemenu, SWT.SEPARATOR);
+	    final MenuItem exitItem = new MenuItem(filemenu, SWT.PUSH);
+	    exitItem.setText("E&xit");
+
+	    // create an edit menu and add cut copy and paste items
+	    final MenuItem edit = new MenuItem(m, SWT.CASCADE);
+	    edit.setText("&Edit");
+	    final Menu editmenu = new Menu(sShell, SWT.DROP_DOWN);
+	    edit.setMenu(editmenu);
+	    final MenuItem cutItem = new MenuItem(editmenu, SWT.PUSH);
+	    cutItem.setText("&Cut");
+	    final MenuItem copyItem = new MenuItem(editmenu, SWT.PUSH);
+	    copyItem.setText("Co&py");
+	    final MenuItem pasteItem = new MenuItem(editmenu, SWT.PUSH);
+	    pasteItem.setText("&Paste");
+
+	    //create an options menu and add menu items
+	    final MenuItem options = new MenuItem(m, SWT.CASCADE);
+	    options.setText("&Options");
+	    final Menu optionsmenu = new Menu(sShell, SWT.DROP_DOWN);
+	    options.setMenu(optionsmenu);
+	    final MenuItem checkItem = new MenuItem(optionsmenu, SWT.CHECK);
+	    checkItem.setText("&Checked Option");
+	    final MenuItem optionsseparator = new MenuItem(optionsmenu, SWT.SEPARATOR);
+	    final MenuItem radioItem1 = new MenuItem(optionsmenu, SWT.RADIO);
+	    radioItem1.setText("Radio &One");
+	    final MenuItem radioItem2 = new MenuItem(optionsmenu, SWT.RADIO);
+	    radioItem2.setText("Radio &Two");
+
+	    //create a Window menu and add Child item
+	    final MenuItem window = new MenuItem(m, SWT.CASCADE);
+	    window.setText("&Window");
+	    final Menu windowmenu = new Menu(sShell, SWT.DROP_DOWN);
+	    window.setMenu(windowmenu);
+	    final MenuItem maxItem = new MenuItem(windowmenu, SWT.PUSH);
+	    maxItem.setText("Ma&ximize");
+	    final MenuItem minItem = new MenuItem(windowmenu, SWT.PUSH);
+	    minItem.setText("Mi&nimize");
+
+	    // create a Help menu and add an about item
+	    final MenuItem help = new MenuItem(m, SWT.CASCADE);
+	    help.setText("&Help");
+	    final Menu helpmenu = new Menu(sShell, SWT.DROP_DOWN);
+	    help.setMenu(helpmenu);
+	    final MenuItem aboutItem = new MenuItem(helpmenu, SWT.PUSH);
+	    aboutItem.setText("&About");
 		
 		
 //		/* File */
